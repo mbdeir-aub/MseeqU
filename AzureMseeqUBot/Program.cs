73a053523a19baddf1ab56703a69f6537552c601
mbdeir <mahmoud.bdeir@gmail.com>
2022-04-12 02:39:48 -0400

    changes to testers only, no need for a new build.

--- AzureMseeqUBot/Program.cs
+++ AzureMseeqUBot/Program.cs
@@ -1,6 +1,8 @@
-﻿#define LIVE
+﻿#define LIVENOT
+using Bdeir.FileStorage;
 using Bdeir.Quizzer.Bots;
 using Bdeir.Quizzer.Core;
+using Bdeir.Quizzer.Parsers;
 using System;
 using System.Linq;
 
@@ -9,10 +11,10 @@ namespace QuizzerBot
     class Program
     {
         static readonly string QUIZ_FILE = Environment.GetEnvironmentVariable("QuizFile");
-        static readonly string AZURE_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("BotAzureStorageConnectionString");
-        static readonly string AZURE_CONTAINER_NAME = Environment.GetEnvironmentVariable("BotAzureContainer");
+        static readonly string AZURE_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("Storage_ConnectionString");
+        static readonly string AZURE_CONTAINER_NAME = Environment.GetEnvironmentVariable("Storage_Container");
 
-        static readonly string BOT_TOKEN = Environment.GetEnvironmentVariable("BotToken");
+        static readonly string BOT_TOKEN = Environment.GetEnvironmentVariable("TelegramBot_ChannelId");
         static readonly string BOT_CHANNEL_ID = Environment.GetEnvironmentVariable("BotChannelId");
 
         const AutoRestartOptions AUTO_RESTART = AutoRestartOptions.AutoRestart;
@@ -23,8 +25,18 @@ namespace QuizzerBot
         const string WRONG_ANSWER_PREFIX = "-";
         static void Main(string[] args)
         {
-            var mcq = Bdeir.Quizzer.MseeqU.CreateAzureQuiz(QUIZ_FILE, AZURE_STORAGE_CONNECTION_STRING, AZURE_CONTAINER_NAME, QUESTION_PREFIX, CORRECT_ANSWER_PREFIX, WRONG_ANSWER_PREFIX, AUTO_RESTART, QUESTIONS_CAN_REPEAT);
-            IQuestion question = mcq.Next();
+
+            #region Method 1
+            IQuestionPicker mcq = Bdeir.Quizzer.MseeqU.CreateAzureQuiz(QUIZ_FILE, AZURE_STORAGE_CONNECTION_STRING, AZURE_CONTAINER_NAME, QUESTION_PREFIX, CORRECT_ANSWER_PREFIX, WRONG_ANSWER_PREFIX, AUTO_RESTART, QUESTIONS_CAN_REPEAT);
+            IQuestion question = mcq.Next(); 
+            #endregion
+
+            #region Method 2
+            var storageConfig = new AzureStorageConfig(AZURE_STORAGE_CONNECTION_STRING, AZURE_CONTAINER_NAME, QUIZ_FILE);
+            var parserConfig = new ParserConfig(QUESTION_PREFIX, CORRECT_ANSWER_PREFIX, WRONG_ANSWER_PREFIX);
+            var storage = new AzureStorage(storageConfig);
+            IQuiz quiz = new QuizFactory().CreateQuizFromFile(QUIZ_FILE, storage, new SingleLineQuizParser(parserConfig));
+            #endregion
 #if LIVE
             TelegramBot bot = new TelegramBot(BOT_TOKEN, BOT_CHANNEL_ID);
             if (bot.SendQuestion(question) != null)
@@ -33,7 +45,7 @@ namespace QuizzerBot
             }
 #else
             Console.WriteLine($"{question.SequenceNumber}: {question.Prompt}");
-            Console.WriteLine(question.ShuffledAnswers.Where(p=>p.Correct));
+            Console.WriteLine(question.ShuffledAnswers.Where(p => p.Correct));
 #endif
         }
     }
--- AzureQuizTester/Program.cs
+++ AzureQuizTester/Program.cs
@@ -12,12 +12,12 @@ namespace QuizTester
         static readonly string WrongAnswerPrefix                 = Environment.GetEnvironmentVariable("Quiz_WrongQuestionPrefix");
         static readonly string AzureStorageConnectionString      = Environment.GetEnvironmentVariable("Storage_ConnectionString");
         static readonly string AzureStorageContainerName         = Environment.GetEnvironmentVariable("Storage_Container");
-        static string FileNameAndPath                            = Environment.GetEnvironmentVariable("Storage_QuizFile");
+        static string quizFile                            = Environment.GetEnvironmentVariable("Storage_QuizFile");
         
         static void Main(string[] args)
         {
-            if (string.IsNullOrWhiteSpace(FileNameAndPath)) FileNameAndPath = "test.txt";
-            var mcq = Bdeir.Quizzer.MseeqU.CreateAzureQuiz(FileNameAndPath, AzureStorageConnectionString, AzureStorageContainerName, QuestionPrefix, CorrectAnswerPrefix, WrongAnswerPrefix, autoRestart, questionsCanRepeat);
+            if (string.IsNullOrWhiteSpace(quizFile)) quizFile = "2022-04-12.txt";
+            var mcq = Bdeir.Quizzer.MseeqU.CreateAzureQuiz(quizFile, AzureStorageConnectionString, AzureStorageContainerName, QuestionPrefix, CorrectAnswerPrefix, WrongAnswerPrefix, autoRestart, questionsCanRepeat);
             IQuestion question = mcq.Next();
             Console.WriteLine($"{question.SequenceNumber}:{question.Prompt}");
         }
