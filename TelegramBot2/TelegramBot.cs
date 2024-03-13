30639a0e033216d3b5f3866313575caf2b5829d6
Mahmoud Bdeir <mahmoud.bdeir@gmail.com>
2024-03-13 18:33:48 -0400

    updated to .net 6, untested

---
+++ TelegramBot2/TelegramBot.cs
@@ -0,0 +1,49 @@
+﻿using Bdeir.Quizzer.Core;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Bdeir.Quizzer.Bots
+{
+    public class TelegramBot
+    {
+        ITelegramBotClient botClient;
+        string botChannelId;
+        public TelegramBot(string botToken, string botChannelId)
+        {
+            botClient = new TelegramBotClient(botToken);
+            this.botChannelId = botChannelId;
+            
+        }
+        public Message SendQuestion(IQuestion q)
+        {
+            return botClient.SendPollAsync(
+                 chatId         : this.botChannelId
+                ,type           : Telegram.Bot.Types.Enums.PollType.Quiz
+                ,question       : q.Prompt
+                ,options        : q.ShuffledAnswers.Select(p => p.AnswerText)
+                ,correctOptionId: q.ShuffledAnswers.FindIndex(p => p.Correct)
+            ).Result;
+        }
+        public Message SendQuestion(IQuestion q, int questionNumber)
+        {
+            return botClient.SendPollAsync(
+                 chatId: this.botChannelId
+                , type: Telegram.Bot.Types.Enums.PollType.Quiz
+                , question: $"({ToIndic(questionNumber)}). " + q.Prompt
+                , options: q.ShuffledAnswers.Select(p => p.AnswerText)
+                , correctOptionId: q.ShuffledAnswers.FindIndex(p => p.Correct)
+            ).Result;
+            
+        }
+        public Message SendText(string text)
+        {
+            return botClient.SendTextMessageAsync(chatId: this.botChannelId, text: text).Result;
+        }
+        public static string ToIndic(int num)
+        {
+            string s = string.Empty;
+            num.ToString().ToList().ForEach(c => s += (char)(0x660 + (int)c - 48));
+            return s;
+        }
+    }
+}
